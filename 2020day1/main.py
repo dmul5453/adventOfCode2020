7cd2fab759e32862e5bbdeb452076a1d190e6719
Dustin Mullins <78238219+dmul5453@users.noreply.github.com>
2021-12-20 10:17:37 -0800

    Add files via upload

---
+++ 2020day1/main.py
@@ -0,0 +1,26 @@
+inputs = []
+numbers = []
+answer = 0
+target = 2020
+
+with open('inputDay1') as f:
+    for line in f:
+        inputs.append(line)
+
+
+print(inputs)
+print(len(inputs))
+
+for i in range(len(inputs)):
+    numbers.append(int(inputs[i]))
+
+print(numbers)
+
+for i in range(0, len(numbers)):
+    for j in range(0, len(numbers)):
+
+        if ((numbers[i] + numbers[j]) == target):
+            answer = numbers[i] * numbers[j]
+
+
+print(answer)
